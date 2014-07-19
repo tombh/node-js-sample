b9908e29985bb5ed793363f54fcbe0b7686abc65
Tom Buckley-Houston <tom@tombh.co.uk>
2014-07-19 17:01:54 +0100

    Add Mongo connection with simple insert and find

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ app.get('/', function(request, response) {
   response.send('Hello ' + (process.env.FOO || 'World') + '!')
 })
 
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+if(process.env.MONGODB_URI){
+  app.get('/mongo', function(request, response) {
+    MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+      if(err) throw err;
+
+      var collection = db.collection('test_insert');
+      collection.drop();
+      collection.insert({'Foolish': 'Barometer'}, function(err, docs) {
+        collection.find().toArray(function(err, results) {
+          response.send(results[0]['Foolish']);
+          db.close();
+        });
+      });
+
+    })
+  })
+}
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
